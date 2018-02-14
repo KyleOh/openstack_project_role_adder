377e72e85ffd88e315ea7cd376522833bc4e612c
Jaewook Oh <jwoh95@dcn.ssu.ac.kr>
2018-02-14 15:47:00 +0900

    useradder.sh added error not stack

--- useradder.sh
+++ useradder.sh
@@ -1,2 +1,16 @@
-#!/usr/bin
+#!/bin/bash
+
+LOG_DIR=/opt/stack/
+STACK_UID=1001
+
+
+if [ "$UID" -ne "$STACK_UID" ]
+then
+  echo ""
+  echo "  [ERROR]  You are not a stack user."
+  echo "  Please login as stack. Below command will help you."
+  echo "         sudo su - stack"
+  echo ""
+  exit 1
+fi
 
